86308ee96bcf1a767da02012b2a5057d4a234c49
Paul Owe <paul.owe@live.com>
2021-08-20 01:33:32 -0400

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -7,5 +7,7 @@ gsutil mb -l US gs://$PROJECT_ID
 python3 my_pipeline.py \
   --project=${PROJECT_ID} \
   --region=us-central1 \
+  --output gs://$PROJECT_ID/results/prefix \
   --tempLocation=gs://$PROJECT_ID/temp/ \
+  --max_num_workers=5 \
   --runner=DataflowRunner
