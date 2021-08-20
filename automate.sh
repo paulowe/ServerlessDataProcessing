67e1e6968d0faed3b650ac24e02ea01ad1e61b91
Paul Owe <paul.owe@live.com>
2021-08-20 01:31:11 -0400

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+export PROJECT_ID=$(gcloud config get-value project)
+gsutil mb -l US gs://$PROJECT_ID
+
+# Launch the pipeline
+python3 my_pipeline.py \
+  --project=${PROJECT_ID} \
+  --region=us-central1 \
+  --tempLocation=gs://$PROJECT_ID/temp/ \
+  --runner=DataflowRunner
